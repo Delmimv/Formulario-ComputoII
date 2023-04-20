8d129a6a32a21ab73c01ac74311b61728ebd0a4a
Delmimv <124009348+Delmimv@users.noreply.github.com>
2023-04-19 21:06:19 -0600

    Add files via upload

---
+++ portafolio/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Mi portafolio Computo II</title>
+</head>
+<body>
+    <?php
+    //Imformacion a mostrar
+    $nombre = "Delmi Fidelina Molina Villatoto";
+    $temas = ""
+
+    ?>
+    <h1><?php echo $nombre; ?></h1>
+    <h2><?php echo $temas; ?></h2>
+</body>
+</html>
